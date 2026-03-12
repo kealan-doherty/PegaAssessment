b91777e0e127811cb59e5c691871f94db1088b42
kealan-doherty <kealandoherty183@gmail.com>
2026-03-11 21:04:33 -0400

    basic project structure

---
+++ src/main/java/com/example/PegaAssessment/PegaAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.PegaAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegaAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegaAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PegaAssessment/PegaAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PegaAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegaAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
