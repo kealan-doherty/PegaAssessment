547b9a16c4406390a4a733e5e471d3f60383748b
kealan-doherty <kealandoherty183@gmail.com>
2026-03-13 00:09:45 -0400

    add unit test for /getAll route pushing to test new workflow

--- src/test/java/com/example/PegaAssessment/PegaAssessmentApplicationTests.java
+++ src/test/java/com/example/PegaAssessment/PegaAssessmentApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.PegaAssessment;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PegaAssessmentApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testGetAll() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/getAll"))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+			.andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
 	}
 
 }
